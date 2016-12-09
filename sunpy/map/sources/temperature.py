3cfa8d473d66e40c0e19ba04f8e77319622448e4
Jack Ireland <jackireland@gmail.com>
2016-12-09 16:19:52 -0500

    improved

--- sunpy/map/sources/temperature.py
+++ sunpy/map/sources/temperature.py
@@ -1,14 +1,12 @@
 """Temperature Map subclass definitions"""
 from __future__ import absolute_import, division, absolute_import
+from sunpy.map import GenericMap
+import matplotlib.cm as cm
+from matplotlib import colors
 #pylint: disable=W0221,W0222,E1101,E1121
 
 __author__ = "Jack Ireland"
 __email__ = "jack.ireland@nasa.gov"
-
-from sunpy.map import GenericMap
-import matplotlib.cm as cm
-from matplotlib import colors
-
 __all__ = ['TemperatureMap']
 
 
@@ -29,7 +27,7 @@ class TemperatureMap(GenericMap):
         # determine the temperature.  Note that this relies on the
         # FITS files having an indication that the file contains a
         # temperature map
-        self.meta['detector'] = 'HK2012'
+        self.meta['detector'] = header.get('DEMMETHO')
         self.meta['obsrvtry'] = None
         self._nickname = self.detector
         # Colour maps
@@ -39,7 +37,7 @@ class TemperatureMap(GenericMap):
     @classmethod
     def is_datasource_for(cls, data, header, **kwargs):
         """Determines if header corresponds to a temperature image."""
-        return header.get('ORIGIN') == 'TEMPERATURE'
+        return header.get('MAPTYPE') == 'TEMPERATURE'
 
     @property
     def measurement(self):
