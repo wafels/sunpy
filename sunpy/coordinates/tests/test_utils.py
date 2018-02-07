b297417b43b7406d365407350ac03a4262d2bb57
Jack Ireland <jackireland@gmail.com>
2018-02-07 11:37:52 -0500

    tests changed to accommodate new great arc

--- sunpy/coordinates/tests/test_utils.py
+++ sunpy/coordinates/tests/test_utils.py
@@ -44,10 +44,8 @@ def test_great_arc_calculable(start, end):
 # Test the calculation of coordinates using varying numbers of points on
 # initialization of the GreatArc object.
 @pytest.mark.parametrize("points_requested, points_expected, first_point, last_point, last_inner_angle, last_distance",
-                         # Test default
-                         [(None, 100, (600, -600), (-100, 800), 1.8683580432741789, 1300377.1981272686),
-                          # Test int as an option
-                          (3, 3, (600, -600), (-100, 800), 1.8683580432741789, 1300377.1981272686),
+                         # Test int as an option
+                         [(3, 3, (600, -600), (-100, 800), 1.8683580432741789, 1300377.1981272686),
                           # Test equally spaced monotonically increasing numpy
                           # array
                           (np.linspace(0, 1, 43), 43, (600, -600), (-100, 800), 1.8683580432741789, 1300377.1981272686),
@@ -56,12 +54,12 @@ def test_great_arc_calculable(start, end):
                           (np.asarray([0.1, 0.2, 0.6, 0.67, 0.99]), 5, (604.68091703, -468.64217597), (-88.83212616, 792.76284375), 1.84967446, 1287373.426146),
                           # Test unequally spaced monotonically decreasing numpy
                           # array
-                          (np.asarray([0.93, 0.78, 0.3, 0.001]), 4, (-21.28208654, 743.58866798), (600.1512768 , -598.78376614), 0.00186836, 1300.37719813),
+                          (np.asarray([0.93, 0.78, 0.3, 0.001]), 4, (-21.28208654, 743.58866798), (600.1512768, -598.78376614), 0.00186836, 1300.37719813),
                           # Test numpy array that increases and decreases
-                          (np.asarray([0.94, 0.73, 0.8, 0.21]), 4, (-32.5852606 , 752.45507707), (585.45829119, -305.26965043), 0.39235519, 273079.21160673)])
+                          (np.asarray([0.94, 0.73, 0.8, 0.21]), 4, (-32.5852606, 752.45507707), (585.45829119, -305.26965043), 0.39235519, 273079.21160673)])
 def test_great_arc_coordinates(points_requested, points_expected, first_point,
                                last_point, last_inner_angle, last_distance):
-
+    print(points_requested, points_expected)
     m = sunpy.map.Map(sunpy.map.Map(sunpy.data.test.get_test_filepath('aia_171_level1.fits')))
     coordinate_frame = m.coordinate_frame
     a = SkyCoord(600*u.arcsec, -600*u.arcsec, frame=coordinate_frame)
@@ -83,13 +81,25 @@ def test_great_arc_coordinates(points_requested, points_expected, first_point,
     assert gc.center.y == 0 * u.km
     assert gc.center.z == 0 * u.km
 
-    np.testing.assert_almost_equal(gc._start_cartesian, np.asarray([428721.09135385, -428722.90519236, 341776.09102756]))
-    np.testing.assert_almost_equal(gc._end_cartesian, np.asarray([-71429.52293813, 571439.07124801, 390859.57978693]))
+    # Starting co-ordinate in Cartesian co-ordinates
+    np.testing.assert_almost_equal(gc.cartesian_coordinates[0], np.asarray([428721.09135385, -428722.90519236, 341776.09102756]))
+
+    # Ending co-ordinate in Cartesian co-ordinates
+    np.testing.assert_almost_equal(gc.cartesian_coordinates[-1], np.asarray([-71429.52293813, 571439.07124801, 390859.57978693]))
+
+    # Center co-ordinate  in Cartesian co-ordinates
     np.testing.assert_almost_equal(gc._center_cartesian, np.asarray([0, 0, 0]))
 
+    # Vector to the start point
     np.testing.assert_almost_equal(gc._v1, np.asarray([428721.09135385, -428722.90519236, 341776.09102756]))
+
+    # radius
     np.testing.assert_almost_equal(gc._r, 696000.00000451738)
+
+    # Vector to the end point
     np.testing.assert_almost_equal(gc._v2, np.asarray([-71429.52293813, 571439.07124801, 390859.57978693]))
+
+    # Third vector - lies in the plane formed by the first and second vector
     np.testing.assert_almost_equal(gc._v3, np.asarray([56761.62657985, 466230.70058902, 513637.08158833]))
 
     # Inner angle
@@ -124,6 +134,15 @@ def test_great_arc_coordinates(points_requested, points_expected, first_point,
     assert len(distances) == points_expected
     np.testing.assert_almost_equal(distances[-1].value, last_distance)
 
+    # In these tests, all the co-ordinates are on the visible disk
+    assert np.all(gc.front_or_back) is True
+    assert gc.all_on_front is True
+    assert gc.all_on_back is False
+    assert gc.from_front_to_back_points_index is None
+    assert gc.from_back_to_front_points_index is None
+    np.testing.assert_almost_equal(gc.front_arc_indices, np.arange(0, gc._npoints))
+    assert gc.back_arc_indices is None
+
 
 # Test that the great arc code rejects wrongly formatted points
 @pytest.mark.parametrize("points", [np.asarray([[0, 0.1], [0.2, 0.3]]),
@@ -139,6 +158,3 @@ def test_great_arc_wrongly_formatted_points(points):
         dummy = GreatArc(a, b, points=points)
 
 
-
-# Test the great circle code against calculable quantities
-# TODO: write tests for the great circle code
