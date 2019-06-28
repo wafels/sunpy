0c3a19cb63407463cea827598059185bb4d88f63
Jack Ireland <jackireland@gmail.com>
2019-06-27 21:59:47 -0400

    expanding tests

--- sunpy/physics/tests/test_differential_rotation.py
+++ sunpy/physics/tests/test_differential_rotation.py
@@ -177,31 +177,39 @@ def test_differential_rotate(aia171_test_map, all_off_disk_map, all_on_disk_map,
         dmap = differential_rotate(all_off_disk_map)
 
     # Test a full disk map
-    new_observer = get_earth(aia171_test_map.date + 6*u.hr)
-    dmap = differential_rotate(aia171_test_map, observer=new_observer)
-    assert dmap.data.shape == aia171_test_map.data.shape
+    dt = 6*u.hr
+    observer = get_earth(aia171_test_map.date + dt)
+    for keys in ({"observer": observer, "time": None}, {"observer": None, "time": dt}):
+        dmap = differential_rotate(aia171_test_map, **keys)
+        assert dmap.data.shape == aia171_test_map.data.shape
 
     # Test a map that is entirely on disk - triggers sub full disk branches
     # Rotated map should have a smaller extent in the x - direction
-    new_observer = get_earth(all_on_disk_map.date - 48*u.hr)
-    dmap = differential_rotate(all_on_disk_map, observer=new_observer)
-    assert dmap.data.shape[1] < all_on_disk_map.data.shape[1]
+    dt = - 48*u.hr
+    observer = get_earth(all_on_disk_map.date + dt)
+    for keys in ({"observer": observer, "time": None}, {"observer": None, "time": dt}):
+        dmap = differential_rotate(all_on_disk_map, **keys)
+        assert dmap.data.shape[1] < all_on_disk_map.data.shape[1]
 
     # This rotated map should have a larger extent in the x direction
-    new_observer = get_earth(all_on_disk_map.date + 48*u.hr)
-    dmap = differential_rotate(all_on_disk_map, observer=new_observer)
-    assert dmap.data.shape[1] > all_on_disk_map.data.shape[1]
+    dt = 48 * u.hr
+    observer = get_earth(all_on_disk_map.date + dt)
+    for keys in ({"observer": observer, "time": None}, {"observer": None, "time": dt}):
+        dmap = differential_rotate(all_on_disk_map, **keys)
+        assert dmap.data.shape[1] > all_on_disk_map.data.shape[1]
 
     # Test a map that straddles the limb - triggers sub full disk branches
     # Rotated map should have a smaller extent in the x - direction
-    new_observer = get_earth(straddles_limb_map.date + 48*u.hr)
-    dmap = differential_rotate(straddles_limb_map, observer=new_observer)
-    assert dmap.data.shape[1] < straddles_limb_map.data.shape[1]
+    dt = 48 * u.hr
+    observer = get_earth(straddles_limb_map.date + dt)
+    for keys in ({"observer": observer, "time": None}, {"observer": None, "time": dt}):
+        dmap = differential_rotate(straddles_limb_map, **keys)
+        assert dmap.data.shape[1] < straddles_limb_map.data.shape[1]
 
     # The output map should have the positional properties of the observer
-    assert dmap.date == new_observer.obstime
-    assert dmap.heliographic_latitude == new_observer.lat
-    assert dmap.heliographic_longitude == new_observer.lon
+    assert dmap.date == observer.obstime
+    assert dmap.heliographic_latitude == observer.lat
+    assert dmap.heliographic_longitude == observer.lon
 
 
 # Tests of the helper functions
